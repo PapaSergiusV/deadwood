b1ef2c8710c5145fb2fc5114b02b644457e69bab
Сергей Пономарев <31237530+PapaSergiusV@users.noreply.github.com>
2018-04-12 23:43:11 +0300

    v 0.1.2

--- common.js
+++ common.js
@@ -4,6 +4,36 @@ var frame = 0;
 var screen = document.getElementById('screen');
 var ctx = screen.getContext('2d');
 
+var entity = {
+	entityImg: new Image(),
+	x: 0,
+	y: 0,
+	h: 0,
+	w: 0,
+	init: function(image, x, y, w, h) {
+		this.entityImg.src = image;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	},
+	draw: function() {
+		ctx.beginPath();	
+		ctx.drawImage(this.entityImg, this.x, this.y - this.h, this.w, this.h);
+		ctx.stroke();
+	}
+}
+
+var player = {
+	moving: function(direction) {
+		if (direction == 'r')
+			this.x += 5;
+		else if (direction == 'l')
+			this.x -= 5;
+	}
+}
+player.__proto__ = entity;
+
 var wood = {
 	woodImg: new Image(),
 	draw: function(image) {
@@ -41,11 +71,31 @@ var ground = {
 
 ground.initGrass('../img/grass.jpg');
 ground.initRelief();
+player.init('../img/player.png', 0, 336, 32, 64);
 
 function main() {
 	wood.draw('../img/wood.jpg');
 	ground.drawRelief();
+	player.draw();
 	frame++;
 }
 
-if (ctx) setInterval(main, 1000);
+if (ctx) setInterval(main, 10);
+
+function moveRect(e){
+    switch(e.keyCode){
+        
+        case 37:  // если нажата клавиша влево
+            player.moving('l');
+            break;
+        case 38:   // если нажата клавиша вверх
+            break;
+        case 39:   // если нажата клавиша вправо
+            player.moving('r');
+            break;
+        case 40:   // если нажата клавиша вниз
+            break;
+    }
+}
+ 
+addEventListener("keydown", moveRect);
